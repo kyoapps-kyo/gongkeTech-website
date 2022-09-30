9064c060456651d6bd5642c0117d1118896fea46
kyoapps-kyo <kyo@kyoapps.com>
2022-09-30 13:15:38 +0900

    mobile nav picture

--- components/Slogan.vue
+++ components/Slogan.vue
@@ -1,5 +1,5 @@
 <template>
   <nuxt-link to="/" class="my-20">
-    <nuxt-img src="logo-slogan-w.png" width="150px"></nuxt-img>
+    <nuxt-picture src="logo-slogan-w.png" width="150px"></nuxt-picture>
   </nuxt-link>
 </template>
